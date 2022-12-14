f9b935725f5fde13367301f24cb8b897b47b5004
Bogdan Bojan <bogdanbojan03@gmail.com>
2022-12-14 08:23:43 +0200

    Add support for reading EPUB format

--- pkg/reader/highlights.go
+++ pkg/reader/highlights.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"bytes"
-	"io"
-
-	pdfcpu "github.com/pdfcpu/pdfcpu/pkg/api"
-)
-
-// pdfcpu "github.com/pdfcpu/pdfcpu/pkg/api"
-
 func main() {
-	// err := exportImages("test-book2.pdf")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	Annotations("test-book2.pdf")
 
 }
-
-func Annotations(name string) {
-	pdfcpu.DecryptFile(name, "cmd", nil)
-}
-
-// func exportImages(name string) error {
-// 	i, s, err := pdfcpu.ListAnnotationsFile(name, nil, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	fmt.Println(i)
-// 	fmt.Println(s)
-
-// 	return nil
-// }
