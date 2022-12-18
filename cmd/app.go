f2f90cf8d79ad49093059879d393bca0e42e7815
Bogdan Bojan <bogdanbojan03@gmail.com>
2022-12-18 15:00:32 +0200

    Create the logic in order to read from an EPUB chapter

--- cmd/app.go
+++ cmd/app.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	"github.com/bogdanbojan/semantick/pkg/epub"
+)
+
+func main() {
+	bk, err := epub.Open("book_test.epub")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bk.Close()
+
+	// log.Printf("files: %+v", bk.Files())
+	s, _ := json.MarshalIndent(bk.Opf.Manifest[2].ID, "", "\t")
+	log.Println(string(s))
+
+	r, err := bk.Open("9780698163744_EPUB-13.xhtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := new(strings.Builder)
+	_, err = io.Copy(buf, r)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(buf.String())
+}
